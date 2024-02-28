99e7d1ea0d8604c1de2198491a0acffa08209722
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-28 17:18:21 +0200

    minor changed

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from bs4 import BeautifulSoup
 url = 'http://quotes.toscrape.com'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_='text')
 
-print(soup)
+print(quotes)
