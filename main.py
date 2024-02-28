d22ebb8a551a757a289c129ef39d7c36c26ffec5
Vitalii Bielimov <vbit2000@gmail.com>
2024-02-28 09:52:09 +0200

    init modules

---
+++ main.py
@@ -0,0 +1,10 @@
+import lxml
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'http://quotes.toscrape.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+
+print(soup)
